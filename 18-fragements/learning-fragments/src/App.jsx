b1534a6873862cea97f13ac47b8c19e1fb1cb299
pratikgoyal9 <ipratikgoyal@yahoo.com>
2025-02-10 11:54:08 +0100

    react-map

--- 18-fragements/learning-fragments/src/App.jsx
+++ 18-fragements/learning-fragments/src/App.jsx
@@ -1,19 +1,35 @@
 import "./App.css";
 
 function App() {
+  const healthyFoods = [
+    "Roti",
+    "Dal",
+    "Rice",
+    "Green Vegetable",
+    "Milk",
+    "Ghee",
+  ];
+
   return (
     /* 
     Without using extra DOM element to wrap more than one element, we can directly wrap under fragments. This fragment won't be loaded inside the DOM
     Use <React.Fragment /> or </> 
     */
+
+    /* 
+    Map is used to convert  from one type to another type. For ex., here we are converting array of string type to list item array.
+
+    use a `key` prop to assign a unique value in virtual DOM, so that rendering of element is seamless when there are updates. Also, using a `key` prop would avoid errors on client console. It gives a performance gain
+    */
+
     <>
       <h1>Healthy Food</h1>
-      <ul class="list-group">
-        <li class="list-group-item">Roti</li>
-        <li class="list-group-item">Dal</li>
-        <li class="list-group-item">Rice</li>
-        <li class="list-group-item">Green vegetables</li>
-        <li class="list-group-item">Milk</li>
+      <ul className="list-group">
+        {healthyFoods.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
