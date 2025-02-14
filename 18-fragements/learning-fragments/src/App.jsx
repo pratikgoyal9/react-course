058bee5379a610c05436710695196cf9fad2f518
pratikgoyal9 <ipratikgoyal@yahoo.com>
2025-02-14 06:51:53 +0100

    add notes

--- 18-fragements/learning-fragments/src/App.jsx
+++ 18-fragements/learning-fragments/src/App.jsx
@@ -1,9 +1,11 @@
 /* 
+    Fragments:
     Without using extra DOM element to wrap more than one element, we can directly wrap under fragments. This fragment won't be loaded inside the DOM
     Use <React.Fragment /> or </> 
     */
 
 /* 
+
     Map is used to convert  from one type to another type. For ex., here we are converting array of string type to list item array.
 
     use a `key` prop to assign a unique value in virtual DOM, so that rendering of element is seamless when there are updates. Also, using a `key` prop would avoid errors on client console. It gives a performance gain
